595f66efc16a357d4f2c2c0f617d753fdf48b2bc
UTakuto <taku.0307.ue@icloud.com>
2024-12-25 00:44:41 +0900

    バンド投票フォームにチェックボックスコンポーネントを追加

---
+++ src/components/BandCheckbox.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface BandCheckboxProps {
+    id: string;
+    label: string;
+    name: string;
+    value: string;
+}
+
+const BandCheckbox: React.FC<BandCheckboxProps> = ({ id, label, name, value }) => {
+    return (
+        <div className="w-full flex items-center ps-4 mt-2 border border-gray-300 rounded-md dark:border-[#fefefe] dark:bg-[#fefefe]">
+            <input
+                id={id}
+                type="checkbox"
+                value={value}
+                name={name}
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label
+                htmlFor={id}
+                className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-[#212121]"
+            >
+                {label}
+            </label>
+        </div>
+    );
+};
+
+export default BandCheckbox;
