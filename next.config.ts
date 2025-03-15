f6ee62948c2942cd22cc32af5c1b38859ad55af0
UTakuto <taku.0307.ue@icloud.com>
2025-03-16 04:04:54 +0900

    feat: webpack設定を更新し、クライアントおよびサーバーサイドのフォールバックを追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            // クライアントサイドの設定
+            config.resolve.fallback = {
+                ...config.resolve.fallback,
+                fs: false,
+                net: false,
+                tls: false,
+                dns: false,
+                child_process: false,
+                process: require.resolve("process/browser"),
+            };
+        }
+
+        if (isServer) {
+            // サーバーサイドの設定
+            config.resolve = {
+                ...config.resolve,
+                fallback: {
+                    ...config.resolve?.fallback,
+                    "node:process": false,
+                },
+            };
+        }
+        return config;
+    },
+    serverExternalPackages: ["firebase-admin"],
 };
 
 export default nextConfig;
